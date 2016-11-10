c19311865811bbd1296d45217dc98f518c161151
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-10 10:37:23 +0100

    boletin 9

---
+++ boletin9_4/src/boletin9_4/Boletin9_4.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9_4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Boletin9_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int e;
+        Tabla tabla=new Tabla();
+        do{
+        e=Integer.parseInt(JOptionPane.showInputDialog("numero"));
+        tabla.multiplicar(e);
+        }while (e!=0);
+
+    }
+    
+}
---
+++ boletin9_4/src/boletin9_4/Tabla.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9_4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Tabla {
+        public void multiplicar (int e){
+        int i=1;
+        if (e!=0)
+        for (i=1;i<=10;i++){
+            System.out.println(e+"x"+i + "="+ (e*i));
+    
+        }
+        }
+}
