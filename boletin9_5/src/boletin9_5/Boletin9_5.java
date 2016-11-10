c6a0b32935774c940f24ccce224d34bda0c37da3
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-10 10:37:18 +0100

    boletin 9

---
+++ boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9_5;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Boletin9_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Series serie= new Series();
+         //serie.serie1();
+         serie.serie2();
+         //serie.serie3();
+    }
+    
+}
---
+++ boletin9_5/src/boletin9_5/Series.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9_5;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Series {
+    public void serie1 () {
+      int e=0;
+      int x=1;
+      int num=Integer.parseInt(JOptionPane.showInputDialog("numero de veces"));
+        do{
+            System.out.println("+"+e);
+            e+=2;
+            x++;
+        }while(x<=num);
+  }
+    public void serie2(){
+        int e=1;
+        int x=Integer.parseInt(JOptionPane.showInputDialog("numero de veces"));
+                do{
+                    System.out.println("+"+e);
+                    e++;
+                    System.out.println("-"+e);
+                    e++;
+                }while (e<=x);
+    }
+    public void serie3(){
+        int e=0;
+        int x=1;
+        int y=1;
+        int num=Integer.parseInt(JOptionPane.showInputDialog("numero de veces"));
+        do{
+            System.out.println(e);
+            y++;
+            if (y<=num)
+            System.out.println(x);
+            y++;
+            e=x+e;
+            x=x+e;
+        }while(y<=num);
+    }
+}
+
