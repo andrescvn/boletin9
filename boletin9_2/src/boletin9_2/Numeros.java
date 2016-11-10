ab1a407cf357d5292a9e28292eb77ceadfce8ef5
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-10 10:37:41 +0100

    boletin 9

---
+++ boletin9_2/src/boletin9_2/Numeros.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9_2;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Numeros {
+    public void suma (){
+       float x = 11;
+       float suma=0;
+         do{
+         suma=suma+x;
+         x++;
+         }while (10<x&&x<90);
+         System.out.println("suma= "+suma);   
+    }
+    public void producto (){
+    float x=6;
+    float producto=1;
+         do{
+         producto=producto*x;
+         x++;
+         }while (5<x&&x<10);
+         System.out.println("producto= "+producto); 
+}
+}
