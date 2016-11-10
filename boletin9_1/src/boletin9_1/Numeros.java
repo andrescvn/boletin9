3e300d28e0a1d9d7102dc2f703e421eb8be18eb8
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-11-10 10:37:46 +0100

    boletin 9

---
+++ boletin9_1/src/boletin9_1/Numeros.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin9_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Numeros {
+   public void Numeros (){
+        int num=0;
+        int x;
+        do{
+        x=Integer.parseInt( JOptionPane.showInputDialog("numero:"));
+        if (x>0)
+            System.out.println("positivo");
+        else 
+            if (x<0)
+                System.out.println("negativo");
+            else 
+                System.out.println("cero");
+        num++;
+        }while (num<10);
+   }
+    
+}
